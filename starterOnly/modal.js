6f06fe9021cdfb3503fc2a80169ddc760004b859
michael.chromebook <michael.discazeaux@gmail.com>
2022-03-21 22:34:00 +0100

    ajout détection validité champ firstname

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,21 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll(".close"); 
 
+// form elements
+const inputFirstName = document.getElementById("first");
+const inputLastName = document.getElementById("last");
+const inputEmail = document.getElementById("email");
+const inputBirthDate = document.getElementById("birthdate");
+
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const nameRegex = /^[A-Za-z_-]{2,30}$/;
+const birthRegex = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+const tournoiRegex = /^[+]?\d+([.]\d+)?$/;
+
+const errorFirst = document.getElementById("errorFirst");
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -26,4 +41,20 @@ function launchModal() {
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+
+// fonction validate
+function validate(event){
+  event.preventDefault();
+  const firstNameValue = inputFirstName.value.trim();
+  if(firstNameValue.length < 2 || nameRegex.test(firstNameValue) == false){
+    errorFirst.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
+    errorFirst.style.fontSize = "13px"
+    errorFirst.style.color = "red"
+    inputFirstName.style.border = "2px solid red"
+  } else {
+    errorFirst.innerText = ""
+    inputFirstName.style.border = "2px solid green"
+  }
+  
 }
