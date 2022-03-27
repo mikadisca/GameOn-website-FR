01a7ea20437b660d559853d17cba8af613796388
michael.chromebook <michael.discazeaux@gmail.com>
2022-03-27 14:35:41 +0200

    ajout des messages validation et erreur sauf quantité de tournois

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,6 +19,9 @@ const inputLastName = document.getElementById("last");
 const inputEmail = document.getElementById("email");
 const inputBirthDate = document.getElementById("birthdate");
 
+const checkBoxLocations = document.getElementsByName("location");
+const checkBoxCGU = document.getElementById("checkbox1");
+
 // regex
 const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 const nameRegex = /^[A-Za-z_-]{2,30}$/;
@@ -29,7 +32,10 @@ const tournoiRegex = /^[+]?\d+([.]\d+)?$/;
 const errorFirst = document.getElementById("errorFirst");
 const errorLast = document.getElementById("errorLast");
 const errorEmail = document.getElementById("errorEmail");
-const errorBirthDate = document.getElementById("errorDate");
+const errorBirthDate = document.getElementById("errorBirthDate");
+
+const errorLocation = document.getElementById("errorLocation");
+const errorCGU = document.getElementById("errorCGU");
 
 
 // launch modal event
@@ -54,7 +60,7 @@ function validate(event){
   // firstname
   const firstNameValue = inputFirstName.value.trim();
   if(firstNameValue.length < 2 || nameRegex.test(firstNameValue) == false){
-    errorFirst.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
+    errorFirst.innerText = " Veuillez entrer 2 caractères ou plus pour le champ du prénom. "
     errorFirst.style.fontSize = "13px"
     errorFirst.style.color = "red"
     inputFirstName.style.border = "2px solid red"
@@ -66,7 +72,7 @@ function validate(event){
   // lastname
   const lastNameValue = inputLastName.value.trim();
   if(lastNameValue.length < 2 || nameRegex.test(lastNameValue) == false){
-    errorLast.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
+    errorLast.innerText = " Veuillez entrer 2 caractères ou plus pour le champ du nom. "
     errorLast.style.fontSize = "13px"
     errorLast.style.color = "red"
     inputLastName.style.border = "2px solid red"
@@ -78,7 +84,7 @@ function validate(event){
   // email
   const emailValue = inputEmail.value.trim();
   if(emailRegex.test(emailValue) == false){
-    errorEmail.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
+    errorEmail.innerText = " Veuillez entrer une adresse email valide "
     errorEmail.style.fontSize = "13px"
     errorEmail.style.color = "red"
     inputEmail.style.border = "2px solid red"
@@ -90,7 +96,7 @@ function validate(event){
   // birthdate
   const birthDateValue = inputBirthDate.value.trim();
   if(birthRegex.test(birthDateValue) == false){
-    errorBirthDate.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
+    errorBirthDate.innerText = "Vous devez entrer votre date de naissance. "
     errorBirthDate.style.fontSize = "13px"
     errorBirthDate.style.color = "red"
     inputBirthDate.style.border = "2px solid red"
@@ -100,7 +106,7 @@ function validate(event){
   }
 
   // quantity tournois
-  const quantityTournoisValue = inputBirthDate.value.trim();
+  /* const quantityTournoisValue = inputBirthDate.value.trim();
   if(birthRegex.test(birthDateValue) == false){
     errorBirthDate.innerText = " Veuillez entrer deux caractères ou plus et respectez le format "
     errorBirthDate.style.fontSize = "13px"
@@ -109,6 +115,26 @@ function validate(event){
   } else {
     errorBirthDate.innerText = ""
     inputBirthDate.style.border = "2px solid green"
+  } */
+
+  // location tournois
+  const inputLocationValue = document.querySelector( 'input[name="location"]:checked')
+  if(inputLocationValue == null){
+    errorLocation.innerText = "Vous devez choisir une option."
+    errorLocation.style.fontSize = "13px"
+    errorLocation.style.color = "red"
+  } else {
+    errorLocation.innerText = ""
+  } 
+
+  // CGU
+  if(checkBoxCGU.checked != true){
+    errorCGU.innerText = "Vous devez vérifier que vous acceptez les termes et conditions. "
+    errorCGU.style.fontSize = "13px"
+    errorCGU.style.color = "red"
+  } else {
+    errorLocation.innerText = ""    
   }
+
 
 }
